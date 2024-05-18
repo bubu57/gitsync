a1edaacd5f622a6982aec8da886f9f45a26754b5
bunelierpro <bunelierpro@gmail.com>
2024-05-18 16:19:37 +0000

    up

--- gitsync.py
+++ gitsync.py
@@ -29,7 +29,11 @@ logger.addHandler(console_handler)
 
 def load_repos(file_path):
     with open(file_path, 'r') as file:
-        return json.load(file)['repos']
+        return json.load(file)
+
+def save_repos(file_path, data):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
 
 def update_repo(repo_info):
     path = repo_info['path']
@@ -70,35 +74,50 @@ def check_commit_pattern(repo_info, pattern):
     return False
 
 def main():
-    parameters = 0
+    file_path = 'data/repos.json'
     
     while True:
-        repos = load_repos('data/repos.json')
+        data = load_repos(file_path)
+        repos = data['repos']
 
         for repo_info in repos:
+            parameters = 0
+            update_needed = False
+
             if repo_info.get('UInt'):
-                parameters = parameters + 1
+                parameters += 1
                 logger.info(f"Scheduled update for repo {repo_info['name']} due to UInt parameter.")
                 update_repo(repo_info)
                 interval = int(repo_info['UInt'])
                 time.sleep(interval * 60)
+                update_needed = True
 
             if repo_info.get('UlastPush'):
-                parameters = parameters + 1
+                parameters += 1
                 if check_new_push(repo_info):
                     logger.info(f"New push detected for repo {repo_info['name']}. Updating...")
                     update_repo(repo_info)
+                    update_needed = True
             
             if repo_info.get('UpatCom'):
-                parameters = parameters + 1
+                parameters += 1
                 pattern = repo_info['UpatCom']
                 if check_commit_pattern(repo_info, pattern):
                     logger.info(f"Commit pattern '{pattern}' found for repo {repo_info['name']}. Updating...")
                     update_repo(repo_info)
+                    update_needed = True
+
+            if update_needed:
+                # Fetch the new SHA of the latest commit
+                repo = Repo(repo_info['path'])
+                latest_commit_sha = repo.commit(repo_info['branch']).hexsha
+                repo_info['lastCommitSha'] = latest_commit_sha
 
             print(repo_info.get('name'), parameters)
-            parameters = 0
-        
+
+        # Save updated repo info back to the JSON file
+        save_repos(file_path, data)
+
         time.sleep(5)  # Wait for 5 seconds before next check
 
 if __name__ == "__main__":
