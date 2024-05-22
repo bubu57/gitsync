cbf306fcf431815a5a13e4eada481de6e9e438b9
bunelierpro <bunelierpro@gmail.com>
2024-05-22 08:53:42 +0000

    up

--- scripts/ubuntu_setup.sh
+++ scripts/ubuntu_setup.sh
@@ -1,10 +1,15 @@
-apt update -y
-apt upgrade -y
-apt install python3 -y
-apt install pip -y
-apt install git -y
-pip3 install Gitpython
-pip3 install threaded
-pip3 install requests
+echo "System update..."
+apt update -y > /dev/null
+apt upgrade -y > /dev/null
+
+echo "Install required packages..."
+apt install python3 -y > /dev/null
+apt install pip -y > /dev/null
+apt install git -y > /dev/null
+
+echo "Install required python packages..."
+pip3 install Gitpython > /dev/null
+pip3 install threaded > /dev/null
+pip3 install requests > /dev/null
 
 cd /gitsync/scripts && python3 gitsync.py
