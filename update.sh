363611654f722e7984e44344a2d8a5d6e90ad971
bunelierpro <bunelierpro@gmail.com>
2024-05-23 20:43:56 +0000

    up

---
+++ update.sh
@@ -0,0 +1,25 @@
+cd web/frontend/build 
+npm run build
+
+cd ../../
+
+rm -fr ../tmp/*
+
+cp -fr web/backend ../tmp 
+cp -fr web/frontend/build ../tmp
+cp -fr scripts ../tmp
+cp -fr docker-compose.yml ../tmp
+cp -fr dockerfile.engine ../tmp
+cp -fr dockerfile.web ../tmp
+cp -fr README.md ../tmp
+cp -fr start.sh ../tmp
+
+git checkout main
+
+cp -fr ../tmp/* .
+
+git add * 
+git commit -m "update"
+git push
+
+git checkout dev
