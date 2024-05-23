75492def8173f73f6de69cd01af7c76919173b56
bunelierpro <bunelierpro@gmail.com>
2024-05-23 20:54:50 +0000

    update

---
+++ tmp/start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Vérifie si le dossier 'data' existe
+if [[ ! -d "data" ]]; then
+    mkdir data
+fi
+
+# Vérifie la présence du fichier token.json
+if [[ ! -f "data/token.json" ]]; then
+    echo '{"token":""}' > data/token.json
+fi
+
+# Vérifie la présence du fichier repos.json
+if [[ ! -f "data/repos.json" ]]; then
+    echo '{"repos":[]}' > data/repos.json
+fi
+
+# Vérifie la présence de Docker et Docker Compose
+if ! command -v docker &> /dev/null; then
+    echo "Erreur : Docker n'est pas installé."
+    exit 1
+fi
+
+if ! command -v docker-compose &> /dev/null; then
+    echo "Erreur : Docker Compose n'est pas installé."
+    exit 1
+fi
+
+docker build -f dockerfile.web -t gitsync_web .
+docker build -f dockerfile.engine -t gitsync_engine .
+
+docker-compose up -d
