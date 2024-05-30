f6b9a558955c93174121142e0a224c1850765be7
bunelierpro <bunelierpro@gmail.com>
2024-05-30 18:22:41 +0000

    update

--- start.sh
+++ start.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 
-# Function to display a loading animation
-function display_loading_animation {
-    local sp='/-\|'
-    printf ' '
-    while true; do
-        printf '\b%s' "$sp"
-        sp=${sp#?}${sp%???}
-        sleep 0.1
-    done
-}
-
-# Function to stop the loading animation
-function stop_loading_animation {
-    kill "$1" 2>/dev/null
-    wait "$1" 2>/dev/null
-}
-
-# Startup animation
+# Startup
 echo "Welcome! Launching GitSync..."
 
 # Check if the 'data' directory exists
@@ -48,18 +31,12 @@ fi
 
 # Building the image for the web service with animation
 printf "Building image for the web service... "
-display_loading_animation &
-animation_pid=$!
 docker build -f dockerfile.web -t gitsync_web . > /dev/null
-stop_loading_animation "$animation_pid"
 printf "Done!\n"
 
 # Building the image for the engine with animation
 printf "Building image for the engine... "
-display_loading_animation &
-animation_pid=$!
 docker build -f dockerfile.engine -t gitsync_engine . > /dev/null
-stop_loading_animation "$animation_pid"
 printf "Done!\n"
 
 # Starting services with animation
